7923137ae297affdf9368cd8756a025c54a0da91
Noni Bell <nonibell2020@u.northwestern.edu>
2021-03-03 11:46:59 -0600

    added base variables to pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,3 +9,15 @@ csvpath = os.path.join("..", "Resources", "budget_data.csv")
 # Open the CSV
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+
+#declare months and profits/losses variables
+months = 0
+profit_loss = 0
+
+#use for loop to calculate the total months and net total profits/losses
+for row in csvreader:
+    months += row[0]
+    profit_loss += row[1]
+
+
+
