63821c67d037167f7c1edcd73996cfcabddf03b0
Noni Bell <nonibell2020@u.northwestern.edu>
2021-03-04 16:57:16 -0600

    seemingly fixed issues with .csv call

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,21 +3,23 @@ import csv
 
 
 # set path to .csv file
-csvpath = os.path.join("PyBank","Resources", "budget_data.csv")
+csvpath = os.path.join('PyBank','Resources', 'budget_data.csv')
 
+# create and declare variables for months and total profits 
+months = 0
+profit_loss = 0
 
 # Open the CSV
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-
-#declare months and profits/losses variables
-months = 0
-profit_loss = 0
-
-#use for loop to calculate the total months and net total profits/losses
-for row in csvreader:
-    months += row[0]
-    profit_loss += row[1]
+    
+    # eliminate the header row
+    csv_header = next(csvreader)
+    print(f"Bank: {csv_header}")
+    
+    for row in csvreader:
+         months += row[0]
+        profit_loss += row[1]
 
 #create and calculate variable for avg profits/losses
 avg_pl = profit_loss / len(csvreader)
@@ -25,3 +27,6 @@ avg_pl = profit_loss / len(csvreader)
 print(f'Total Profits: ${profit_loss}')
 print(f'Total Months: {months}')
 print(f'Average Profits: ${avg_pl}')
+
+
+
