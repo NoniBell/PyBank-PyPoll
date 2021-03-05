0d202ce667ee707f27573c7c958b46bb9e6417c7
Noni Bell <nonibell2020@u.northwestern.edu>
2021-03-05 14:12:17 -0600

    finished pybank calculations

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,7 +7,13 @@ csvpath = os.path.join('PyBank','Resources', 'budget_data.csv')
 
 # create and declare variables for months and total profits 
 months = 0
-profit_loss = 0
+profits = 0
+
+# create variables for greatest profit/loss
+greatestp = 0
+greatestl = 0
+greatsp_month = ""
+greatestl_month = ""
 
 # Open the CSV
 with open(csvpath) as csvfile:
@@ -20,22 +26,28 @@ with open(csvpath) as csvfile:
     # use for loop to add all months and profits
     for row in csvreader:
         months += 1
-        profit_loss += int(row[1])
+        profits += int(row[1])
+    # if current row is greater OR less than current variables then set to current row
+        if int(row[1]) > greatestp:
+            greatestp = int(row[1])
+            greatestp_month = row[0]
+        if int(row[1]) < greatestl:
+            greatestl = int(row[1])
+            greatestl_month = row[0]
 
 # create and calculate variable for avg profits/losses
-avg_pl = profit_loss / months
+avg_pl = profits / months
 # round average
 avg_pl = round(avg_pl, 2)
 
-# create variables for greatest profit/loss, incrementer
-# use for loop to find greatest profit/loss
-# if current row is greater OR less than current variables then set to current row
-# increase incrementer regardless
-
 #print all collected variables
-print(f'Total Profits: ${profit_loss}')
+print('Financial Analysis')
+print('---------------------')
 print(f'Total Months: {months}')
+print(f'Total Profits: ${profits}')
 print(f'Average Profits: ${avg_pl}')
+print(f'Greatest Increase in Profits: {greatestp_month} (${greatestp})')
+print(f'Greatest Decrease in Profits: {greatestl_month} (${greatestl})')
 
 
 
